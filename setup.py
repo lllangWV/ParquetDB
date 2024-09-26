d7d7600c35d645074282e66f0beb45e8509412fe
lllangWV <lllangWV@gmail.com>
2024-09-26 15:56:05 -0400

    Updated .gitignore and updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="parquetdb",  
-    version="0.1.0",  
+    version="0.1.1",  
     author="Logan Lang", 
     author_email="lllang@mix.wvu.edu", 
     description="ParquetDB is a lightweight database-like system built on top of Apache Parquet files using PyArrow.",  
