c05e3f2a6956c21d86871914efa8538da635bf0a
lllangWV <lllangWV@gmail.com>
2024-10-01 22:59:00 -0400

    Updated dependency management. Created pyproject.toml and now use setuptools_stm to handle versions

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools_scm import ScmVersion
 
-setup(
-    name="parquetdb",  
-    version="0.5.1",  
-    author="Logan Lang", 
-    author_email="lllang@mix.wvu.edu", 
-    description="ParquetDB is a lightweight database-like system built on top of Apache Parquet files using PyArrow.",  
-    long_description=open("README.md").read(), 
-    long_description_content_type="text/markdown",
-    url="https://github.com/lllangWV/ParquetDB",  # Replace with your package's URL
-    packages=find_packages(),
-    install_requires=[
-        # Add your package dependencies here, e.g.:
-        'pyarrow',
-        'pandas',
-        'numpy',
-    ],
-    extras_require={
-        'dev': [
-            'pytest',
-        ],
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  # Replace with your license
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",  # Specify the minimum Python version required
-)
+def version_for_project(version: ScmVersion) -> str:
+   return str(version.tag)
+   
+setup(use_scm_version={'version_scheme':version_for_project})
