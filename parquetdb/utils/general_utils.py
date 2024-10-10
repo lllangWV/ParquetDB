6d99c4f561f3857b9a73d1422539e27690ebe227
lllangWV <lllangWV@gmail.com>
2024-10-10 14:14:52 -0400

    Added more detailed docstrings

--- parquetdb/utils/general_utils.py
+++ parquetdb/utils/general_utils.py
@@ -12,14 +12,24 @@ import pyarrow as pa
 
 def timeit(func):
     """
-    A decorator that measures the execution time of a function.
+    A decorator that measures and logs the execution time of a function.
 
-    Args:
-        func: The function to be timed.
+    Parameters
+    ----------
+    func : function
+        The function whose execution time is to be measured.
 
-    Returns:
-        The wrapped function.
+    Returns
+    -------
+    function
+        The wrapped function with timing capabilities.
 
+    Example
+    -------
+    @timeit
+    def example_function():
+        # Function logic here
+        pass
     """
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -35,13 +45,22 @@ def timeit(func):
 
 def is_directory_empty(directory_path:str):
     """
-    Checks if a directory is empty.
+    Checks if a given directory is empty.
 
-    Args:
-        directory_path (str): The path to the directory.
+    Parameters
+    ----------
+    directory_path : str
+        The path to the directory to check.
 
-    Returns:
-        bool: True if the directory is empty, False otherwise.
+    Returns
+    -------
+    bool
+        True if the directory is empty, False otherwise.
+
+    Example
+    -------
+    >>> is_empty = is_directory_empty('/path/to/directory')
+    True
     """
 
     return not os.listdir(directory_path)
@@ -49,20 +68,52 @@ def is_directory_empty(directory_path:str):
 
 def is_contained(list1, list2):
     """
-    Checks if a list is contained in another list.
+    Checks if all elements of the first list are contained in the second list.
 
-    Args:
-        list1 (list): The first list.
-        list2 (list): The second list.
+    Parameters
+    ----------
+    list1 : list
+        The list whose elements are to be checked.
+    list2 : list
+        The list in which to check for the presence of `list1` elements.
 
-    Returns:
-        bool: True if list1 is contained in list2, False otherwise.
+    Returns
+    -------
+    bool
+        True if all elements of `list1` are contained in `list2`, False otherwise.
+
+    Example
+    -------
+    >>> is_contained([1, 2], [1, 2, 3, 4])
+    True
     """
     return set(list1).issubset(set(list2))
 
 
 
 def generate_similar_data(template_data, num_entries):
+    """
+    Generates new data entries based on the structure and values of a template dataset.
+
+    Parameters
+    ----------
+    template_data : list of dict
+        A list of dictionaries containing the template data to base new entries on.
+    num_entries : int
+        The number of similar data entries to generate.
+
+    Returns
+    -------
+    list of dict
+        A list of generated data entries with variations in the values.
+
+    Example
+    -------
+    >>> template = [{'id': 0, 'value': 10, 'name': 'item'}]
+    >>> new_data = generate_similar_data(template, 5)
+    >>> print(new_data)
+    [{'id': 0, 'value': 9, 'name': 'item_5'}, {'id': 1, 'value': 11, 'name': 'item_89'}, ...]
+    """
     def generate_value(value):
         if isinstance(value, int):
             return random.randint(value - 10, value + 10)
