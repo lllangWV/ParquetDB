126832eaac566d9058b39b271ba36f6e30a22805
lllangWV <lllangWV@gmail.com>
2024-10-25 14:29:57 -0400

    Error. Removed ParquetDb_manager

--- parquetdb/__init__.py
+++ parquetdb/__init__.py
@@ -2,5 +2,4 @@ from parquetdb._version import __version__
 
 from parquetdb.utils.config import config
 
-from parquetdb.core.parquetdb_manager import ParquetDBManager
 from parquetdb.core.parquetdb import ParquetDB
