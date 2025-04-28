7e1af2e811ae4c3b595cc89db760793749e5da49
lllangWV <lllangWV@gmail.com>
2025-04-28 16:33:21 -0400

    chore: Bump version to 0.26.0
    Updated the version number in _version.py to reflect the new release.
    This change is part of the preparation for the upcoming v0.26.0 release.

--- parquetdb/_version.py
+++ parquetdb/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.25.1'
-__version_tuple__ = version_tuple = (0, 25, 1)
+__version__ = version = '0.26.0'
+__version_tuple__ = version_tuple = (0, 26, 0)
