bbb59d184b26166603ed66c3396a727b107f7692
lllangWV <lllangWV@gmail.com>
2025-07-20 12:02:37 -0400

    chore: Remove version scheme from setup.py
    Eliminated the use_scm_version parameter from setup() in setup.py to
    simplify the setup configuration. This change does not affect the
    functionality of the package.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 from setuptools_scm import ScmVersion
 
+
 def version_for_project(version: ScmVersion) -> str:
    return str(version.tag)
    
-setup(use_scm_version={'version_scheme':version_for_project})
+setup()
